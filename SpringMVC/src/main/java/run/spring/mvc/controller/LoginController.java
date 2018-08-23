017afbdc99229243b2d1708d02097f5b1f02db91
hoanghiep194 <hiep.nguyenhoang1904@gmail.com>
2018-08-23 08:38:27 +0700

    update code

--- SpringMVC/src/main/java/run/spring/mvc/controller/LoginController.java
+++ SpringMVC/src/main/java/run/spring/mvc/controller/LoginController.java
@@ -1,17 +1,106 @@
 package run.spring.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import run.spring.mvc.dto.UserDto;
+import run.spring.mvc.services.LoginService;
+
+
 @Controller
+@SessionAttributes("user")
+
 public class LoginController {
 
-    @RequestMapping("/welcome")
-    public ModelAndView helloWorld() {
+    @Autowired
+    private LoginService loginService;
 
-        String message = "<br><div style='text-align:center;'>"
-                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
-        return new ModelAndView("welcome", "message", message);
+    private static final Logger logger = Logger.getLogger(LoginController.class);
+
+    private List<String> list = new ArrayList<>();
+
+    @ModelAttribute("user")
+    public UserDto setUser() {
+       return new UserDto();
+    }
+
+//    @ModelAttribute("user")
+//    public UserDto getUser() {
+//        return new UserDto();
+//
+//    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(@ModelAttribute("user1") UserDto user, Model model) throws Exception {
+        ModelAndView modelView = new ModelAndView("welcome");
+
+        List<UserDto> result = loginService.login(user);
+        model.addAttribute("user", result.get(0));
+        return modelView;
+    }
+//    @RequestMapping("/welcome")
+//    public ModelAndView helloWorld() {
+//
+////        String message = "<br><div style='text-align:center;'>"
+////                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+////        return new ModelAndView("welcome", "message", message);
+//
+////        list = getList();
+//        ModelAndView model = new ModelAndView("welcome");
+//        model.addObject("lists", list);
+//
+//        return model;
+//    }
+//
+//    private List<String> getList() {
+//
+//        List<String> list = new ArrayList<String>();
+//        list.add("Java");
+//        list.add("C++");
+//        list.add("PHP");
+//
+//        return list;
+//
+//    }
+//
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public ModelAndView addBooks(@ModelAttribute("book") Books book) {
+//
+//        logger.info("1111111111111");
+//        String booksName = book.getBooksName();
+//        logger.info("2222222222222 " + booksName);
+//
+//        logger.info("3333333333333 " + book.getSession());
+//
+//        book.getSession().add(booksName);
+//        logger.info("444444444444 " + book.getSession());
+//        ModelAndView model = new ModelAndView("welcome");
+//        model.addObject("lists", book.getSession());
+//
+//        return model;
+//    }
+//
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public ModelAndView userInfo(@SessionAttribute("user") UserDto user) {
+
+//        UserDto user = (UserDto) model.asMap().get("user");
+        System.out.println("UserName : " + user.getUserName());
+        System.out.println("Password : " + user.getPassWord());
+
+        ModelAndView modelview = new ModelAndView("welcome");
+        modelview.addObject("username", user.getUserName());
+
+       return modelview;
     }
 }
--- SpringMVC/target/SpringMVC/WEB-INF/views/welcome.jsp
+++ SpringMVC/target/SpringMVC/WEB-INF/views/welcome.jsp
@@ -1,25 +1,15 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.Date"%>
+<%@page import="run.spring.mvc.model.Books"%>
+
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <html>
-<head>
-<title>Spring MVC Tutorial by Crunchify - Hello World Spring MVC
-	Example</title>
-<style type="text/css">
-body {
-	background-image: url('https://cdn.crunchify.com/bg.png');
-}
-</style>
-</head>
-<body>${message}
-
-	<br>
-	<br>
-	<div
-		style="font-family: verdana; padding: 10px; border-radius: 10px; font-size: 12px; text-align: center;">
-
-		Spring MCV Tutorial by <a href="https://crunchify.com">Crunchify</a>.
-		Click <a href="https://crunchify.com/category/java-tutorials/"
-			target="_blank">here</a> for all Java and <a
-			href='https://crunchify.com/category/spring-mvc/' target='_blank'>here</a>
-		for all Spring MVC, Web Development examples.<br>
-	</div>
+<body>
+    Hello
+     <a href="${pageContext.request.contextPath}/info">View profile</a>
+</br>
+Session of User : <c:out value="${username}"></c:out>
 </body>
 </html>
